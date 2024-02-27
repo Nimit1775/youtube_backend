6f675a073b529769f10c1181d8240fdeb71c6a5a
nimit sodhani <nimitsodhani1@gmail.com>
2024-02-27 18:47:23 +0530

    database-running

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,24 @@
 import mongoose from "mongoose";
 import { DBNAME } from "../constants.js";
 
+const connectDB = async () => {
+    try {
+      const connection = await mongoose.connect(
+        "mongodb+srv://nimitsodhani1:nimit@youtube.sojnuol.mongodb.net"
+      );
+  
+      console.log(`\n MongoDB connected !! DB HOST : $
+              {connectionInstance.connection.host} `);
+    } catch (error) {
+      console.log("MONGODB connection error: ", error);
+      process.exit(1);
+    }
+  };
 
+
+
+  
+/*
 const connectDB = async () => {
 
     try {
@@ -18,6 +35,9 @@ const connectDB = async () => {
 
     }
 
-}
+}*/
 
 export default connectDB;
+  
+  
+  
--- src/index.js
+++ src/index.js
@@ -4,28 +4,13 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+
 dotenv.config({
-    path: './.env.'
+
+    path:'./env'
 })
-
-
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
-
-
-
-
-
-
-
 
 
 
