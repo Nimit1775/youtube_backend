f74a2184f4ff06223720eee237fe4e5bbefa79a8
nimit sodhani <nimitsodhani1@gmail.com>
2024-02-27 20:23:58 +0530

    adding CORS and cookie-parser

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
 
@@ -11,6 +12,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(` server is running at port :$
+        {process.env.PORT}`);
+    })
+}) 
+.catch((err)=>{
+    console.log("MONGO db connection error", err);
+})
 
 
 
